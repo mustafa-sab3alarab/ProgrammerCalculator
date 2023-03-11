93e8c4ab4c99da465ef156c046c4acca54feb3d1
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-03-11 13:57:15 +0100

    improve edit text to take only 8 char

--- app/src/main/java/com/example/programmercalculator/Converter.kt
+++ app/src/main/java/com/example/programmercalculator/Converter.kt
@@ -1,81 +1,92 @@
 package com.example.programmercalculator
 
+import android.util.Log
+
 object Converter {
 
-    fun binaryToDecimal(value: Int): String {
+    fun binaryToDecimal(value: Long): String {
         try {
             return Integer.parseInt(value.toString(), 2).toString()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C1", exception.message.toString())
         }
+        return "0"
     }
 
-    fun binaryToOctal(value: Int): String {
+    fun binaryToOctal(value: Long): String {
         try {
             return Integer.toOctalString(binaryToDecimal(value).toInt())
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C2", exception.message.toString())
         }
+        return "0"
     }
 
-    fun binaryToHexadecimal(value: Int): String {
+    fun binaryToHexadecimal(value: Long): String {
         try {
             return Integer.toHexString(binaryToDecimal(value).toInt()).uppercase()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C3", exception.message.toString())
         }
+        return "0"
     }
 
 
-    fun decimalToBinary(value: Int): String {
+    fun decimalToBinary(value: Long): String {
         try {
-            return Integer.toBinaryString(value).toString()
+            return Integer.toBinaryString(value.toInt()).toString()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C4", exception.message.toString())
         }
+        return "0"
     }
 
-    fun decimalToOctal(value: Int): String {
+    fun decimalToOctal(value: Long): String {
         try {
-            return Integer.toOctalString(value).toString()
+            return Integer.toOctalString(value.toInt()).toString()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C5", exception.message.toString())
         }
+        return "0"
 
     }
 
-    fun decimalToHexadecimal(value: Int): String {
+    fun decimalToHexadecimal(value: Long): String {
         try {
-            return Integer.toHexString(value).uppercase()
+            return Integer.toHexString(value.toInt()).uppercase()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C6", exception.message.toString())
         }
+        return "0"
 
     }
 
 
-    fun octalToBinary(value: Int): String {
+    fun octalToBinary(value: Long): String {
         try {
             return Integer.toBinaryString(octalToDecimal(value).toInt()).toString()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C7", exception.message.toString())
         }
+        return "0"
     }
 
-    fun octalToDecimal(value: Int): String {
+    fun octalToDecimal(value: Long): String {
         try {
             return Integer.parseInt(value.toString(), 8).toString()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C8", exception.message.toString())
         }
+        return "0"
     }
 
-    fun octalToHexadecimal(value: Int): String {
+    fun octalToHexadecimal(value: Long): String {
         try {
             return Integer.toHexString(octalToDecimal(value).toInt()).toString().uppercase()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C9", exception.message.toString())
         }
+        return "0"
     }
 
 
@@ -83,23 +94,26 @@ object Converter {
         try {
             return Integer.toBinaryString(hexadecimalToDecimal(value).toInt())
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C10", exception.message.toString())
         }
+        return "0"
     }
 
     fun hexadecimalToDecimal(value: String): String {
         try {
             return Integer.parseInt(value, 16).toString()
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C11", exception.message.toString())
         }
+        return "0"
     }
 
     fun hexadecimalToOctal(value: String): String {
         try {
             return Integer.toOctalString(hexadecimalToDecimal(value).toInt())
         } catch (exception: NumberFormatException) {
-            throw exception
+            Log.e("C12", exception.message.toString())
         }
+        return "0"
     }
 }
